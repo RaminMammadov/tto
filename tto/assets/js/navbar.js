bbf6e06f300bbc334c61dc83fccc89e282d70a12
RaminMammadov <50649777+RaminMammadov@users.noreply.github.com>
2021-05-12 08:31:41 -0700

    Update navbar.js

--- tto/assets/js/navbar.js
+++ tto/assets/js/navbar.js
@@ -15,20 +15,27 @@ function closeMenu() {
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-desktop");
 
-hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
 
 var dropBtn = document.getElementsByClassName('dropdown');
 for (let i = 0;i<=dropBtn.length - 1;i++) {
    dropBtn[i].addEventListener('click',(e) => {
        e.target.parentElement.children[1].classList.toggle('show');
+       console.log( e.target.parentElement.children[1])
        navMenu.classList.toggle("active");
 
        hamburger.classList.toggle("active");
 
    })
+}
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+    let j = document.getElementsByClassName('subMenu');
+    for (let i = 0; i <= j.length - 1;i++) {
+        j[i].classList.remove('show')
+    }
 }
